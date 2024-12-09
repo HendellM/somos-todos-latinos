19bf13b29ef01639adcbc1b9b9304fc25000a890
Hendell Miguel <hendelllmiguel@gmail.com>
2024-12-08 23:30:33 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btnLupa = document.getElementById("btn-lupa");
+    const inputPesquisa = document.getElementById("input-pesquisa");
+    btnLupa.addEventListener("click", () => {
+      inputPesquisa.classList.toggle("show");
+      inputPesquisa.focus();
+    });
+  });
